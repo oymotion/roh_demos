17388ee6f960de947a283fda051220c604cd18ab
Wang Hang <1154690309@qq.com>
2025-02-08 18:33:40 +0800

    Merge 12bits version into glove_ctrled_hand.py

--- glove_ctrled_rohand/glove_ctrled_hand.py
+++ glove_ctrled_rohand/glove_ctrled_hand.py
@@ -10,6 +10,7 @@ from pymodbus import FramerType
 from pymodbus.client import ModbusSerialClient
 
 from lib_gforce import gforce
+from lib_gforce.gforce import EmgRawDataConfig, SampleResolution
 
 from roh_registers_v1 import *
 
@@ -17,15 +18,15 @@ from roh_registers_v1 import *
 COM_PORT = "COM8"
 NODE_ID = 2
 
-# Degree of freedom settings, modify EXTRA_FINGERS to distinguish between 5 DOF and 6 DOFgloves.
-NUM_FINGERS = 5
-EXTRA_FINGERS = 1
+NUM_FINGERS = 6
 
 # Device filters
 DEV_NAME_PREFIX = "gForceBLE"
 DEV_MIN_RSSI = -64
 
-NUM_CHANNELS = NUM_FINGERS + EXTRA_FINGERS
+# sample resolution:BITS_8 or BITS_12
+SAMPLE_RESOLUTION = 8
+
 # Channel0: thumb, Channel1: index, Channel2: middle, Channel3: ring, Channel4: pinky, Channel5: thumb root
 INDEX_CHANNELS = [7, 6, 0, 3, 4, 5]
 
@@ -54,11 +55,11 @@ class Application:
 
     async def main(self):
         gforce_device = gforce.GForce(DEV_NAME_PREFIX, DEV_MIN_RSSI)
-        emg_data = [0 for _ in range(NUM_CHANNELS)]
-        emg_min = [0 for _ in range(NUM_CHANNELS)]
-        emg_max = [0 for _ in range(NUM_CHANNELS)]
-        prev_finger_data = [65535 for _ in range(NUM_CHANNELS)]
-        finger_data = [0 for _ in range(NUM_CHANNELS)]
+        emg_data = [0 for _ in range(NUM_FINGERS)]
+        emg_min = [0 for _ in range(NUM_FINGERS)]
+        emg_max = [0 for _ in range(NUM_FINGERS)]
+        prev_finger_data = [65535 for _ in range(NUM_FINGERS)]
+        finger_data = [0 for _ in range(NUM_FINGERS)]
 
         client = ModbusSerialClient(COM_PORT, FramerType.RTU, 115200)
         client.connect()
@@ -74,6 +75,11 @@ class Application:
 
         print("Connected to {0}".format(gforce_device.device_name))
 
+        # Set the EMG raw data configuration, default configuration is 8 bits, 16 batch_len
+        if SAMPLE_RESOLUTION == 12:
+            cfg = EmgRawDataConfig(fs=100, channel_mask=0xff, batch_len = 8, resolution = SampleResolution.BITS_12)
+            await gforce_device.set_emg_raw_data_config(cfg)
+
         baterry_level = await gforce_device.get_battery_level()
         print("Device baterry level: {0}%".format(baterry_level))
 
@@ -87,10 +93,17 @@ class Application:
             # print(v)
 
             for i in range(len(v)):
-                for j in range(NUM_CHANNELS):
+                for j in range(NUM_FINGERS):
                     emg_max[j] = round((emg_max[j] + v[i][INDEX_CHANNELS[j]]) / 2)
 
         # print(emg_max)
+
+        print("Please rotate thumb root")
+
+        for _ in range(256):
+            v = await q.get()
+            for i in range(len(v)):
+                emg_min[5] = round((emg_min[5] + v[i][INDEX_CHANNELS[5]]) / 2)
 
         print("Please make a fist")
 
@@ -99,23 +112,14 @@ class Application:
             # print(v)
 
             for i in range(len(v)):
-                for j in range(NUM_CHANNELS):
+                for j in range(NUM_FINGERS):
                     emg_min[j] = round((emg_max[j] + v[i][INDEX_CHANNELS[j]]) / 2)
         
-        # For extra fingers
-        if NUM_CHANNELS > NUM_FINGERS:
-            print("Please spread your fingers, then rotate thumb")
-
-            for _ in range(256):
-                v = await q.get()
-                for i in range(len(v)):
-                    emg_min[5] = round((emg_min[5] + v[i][INDEX_CHANNELS[5]]) / 2)
-        
-        # print(emg_min)
+            # print(emg_min)
         
         range_valid = True
 
-        for i in range(NUM_CHANNELS):
+        for i in range(NUM_FINGERS):
             print("MIN/MAX of finger {0}: {1}-{2}".format(i, emg_min[i], emg_max[i]))
             if (emg_min[i] == emg_max[i]):
                 range_valid = False
@@ -130,7 +134,7 @@ class Application:
             # print(v)
 
             for i in range(len(v)):
-                for j in range(NUM_CHANNELS):
+                for j in range(NUM_FINGERS):
                     emg_data[j] = round((emg_data[j] + v[i][INDEX_CHANNELS[j]]) / 2)
                     finger_data[j] = round(interpolate(emg_data[j], emg_min[j], emg_max[j], 65535, 0))
                     finger_data[j] = clamp(finger_data[j], 0, 65535)
